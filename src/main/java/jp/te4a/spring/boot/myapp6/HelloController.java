3cd18f157d5e67bf33a7caff9b9d2bf8ca7c5f02
190738 <190738@e0601-14.JC-21.local>
2022-07-12 12:58:10 +0900

    7/12 3

--- src/main/java/jp/te4a/spring/boot/myapp6/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/HelloController.java
@@ -3,6 +3,7 @@ package jp.te4a.spring.boot.myapp6;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
     @Autowired
     BookService bookService;
-    @RequestMapping("/main")
+    @GetMapping("/main")
     public String index(Model model) {
         model.addAttribute("msg", "this is setting message");
         return "index";
